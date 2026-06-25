18d98199746acd44391c578fd5b9d63bdc2900d0
Yarno <yarno.vandenplas@student.ehb.be>
2026-06-25 16:47:33 +0200

    Added chat panel manager add and removal

--- core/src/main/java/be/yarno/chatpanels/manager/ChatPanelManager.java
+++ core/src/main/java/be/yarno/chatpanels/manager/ChatPanelManager.java
@@ -1,5 +1,36 @@
 package be.yarno.chatpanels.manager;
 
-public class ChatPanelManager {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import be.yarno.chatpanels.panel.ChatPanel;
 
+public class ChatPanelManager {
+  private final List<ChatPanel> panels;
+
+  public ChatPanelManager() {
+    panels = new ArrayList<>();
+  }
+
+  public List<ChatPanel> getChatPanels() {
+    return Collections.unmodifiableList(panels);
+  }
+
+  public void addPanel(ChatPanel panel) {
+    if (panel == null) {
+      throw new IllegalArgumentException();
+    }
+
+    panels.add(panel);
+  }
+
+  public void removePanel(ChatPanel panel) {
+    if (panel == null) {
+      throw new IllegalArgumentException();
+    }
+
+    if (!panels.remove(panel)) {
+      throw new IllegalArgumentException("Panel not found");
+    }
+  }
 }
