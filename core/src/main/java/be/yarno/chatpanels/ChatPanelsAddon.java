a59613bd7e0b51c85739b12f209990724af14552
Yarno <yarno.vandenplas@student.ehb.be>
2026-06-25 23:42:22 +0200

    Added chat message listener

--- core/src/main/java/be/yarno/chatpanels/ChatPanelsAddon.java
+++ core/src/main/java/be/yarno/chatpanels/ChatPanelsAddon.java
@@ -1,14 +1,32 @@
 package be.yarno.chatpanels;
 
+import be.yarno.chatpanels.listener.ChatMessageListener;
+import be.yarno.chatpanels.manager.ChatPanelManager;
 import net.labymod.api.addon.LabyAddon;
 import net.labymod.api.models.addon.annotation.AddonMain;
 
 @AddonMain
 public class ChatPanelsAddon extends LabyAddon<ChatPanelConfiguration> {
 
+  private ChatPanelManager panelManager;
+  private ChatMessageListener messageListener;
+
   @Override
   protected void enable() {
     this.registerSettingCategory();
+
+    panelManager = new ChatPanelManager();
+    messageListener = new ChatMessageListener(panelManager);
+
+    System.out.println("ChatPanels manager loaded!");
+  }
+
+  public ChatPanelManager getPanelManager() {
+    return panelManager;
+  }
+
+  public ChatMessageListener getMessageListener() {
+    return messageListener;
   }
 
   @Override
--- core/src/main/java/be/yarno/chatpanels/listener/ChatMessageListener.java
+++ core/src/main/java/be/yarno/chatpanels/listener/ChatMessageListener.java
@@ -1,5 +1,21 @@
 package be.yarno.chatpanels.listener;
 
+import be.yarno.chatpanels.manager.ChatPanelManager;
+import be.yarno.chatpanels.model.ChatMessage;
+
 public class ChatMessageListener {
 
+  private final ChatPanelManager panelManager;
+
+  public ChatMessageListener(ChatPanelManager panelManager) {
+    this.panelManager = panelManager;
+  }
+
+  public void handleMessage(ChatMessage message) {
+    if (message == null) {
+      throw new IllegalArgumentException();
+    }
+
+    panelManager.handleMessage(message);
+  }
 }
